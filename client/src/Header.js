73f97339f9821a8d0069be859f32ed553f4653ce
sureshnaloor <suresh.naloor@gmail.com>
2020-03-24 12:12:26 +0300

    fo

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import logo from './images/JALLogo.png'
 export default function Header() {
@@ -11,34 +10,3 @@ export default function Header() {
     </div>
     )
 }
-=======
-import React from 'react';
-import logo from './images/JALLogo.png';
-import img1 from './images/img1.jpg';
-import img2 from './images/img2.jpg';
-import img3 from './images/img3.jpg';
-
-export default function Header() {
-  return (
-    <div>
-      <header className='App-header'>
-        <div>
-          <img className='logo' src={logo} />
-          <h4> MM PORTAL - JAL International </h4>
-        </div>
-        <img src={img1} alt='pic1' />
-        <img src={img3} alt='pic2' />
-        <img src={img2} alt='pic3' />
-        <div>
-          <p>
-            {' '}
-            <span>Internal SAP Portal</span>
-          </p>
-          <h4>for estimation and project teams for Material data</h4>
-          <h6> Strictly for internal use only for authorised users</h6>
-        </div>
-      </header>
-    </div>
-  );
-}
->>>>>>> cebe1101cd204bd9855d6b47e61a8b90901e19b8
--- client/src/Mainnav.js
+++ client/src/Mainnav.js
@@ -1,13 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-
-export default function Mainnav() {
-    return (
-        <div> 
-            <nav className="mainnav">
-            <ul className="mainnav-menu">
-=======
 
 import { Link } from 'react-router-dom';
 
@@ -16,7 +7,6 @@ export default function Mainnav() {
         <div>
         <nav className="mainnav">
           <ul className="mainnav-menu">
->>>>>>> cebe1101cd204bd9855d6b47e61a8b90901e19b8
               <li className="mainnav-link"> <Link to="/"> <i className="fa fa-home" aria-hidden="true"></i> Home </Link></li>
               <li className="mainnav-link"> <Link to="/show-matgroups"> <i className="far fa-images"></i>Show matgroups </Link></li>
               <li className="mainnav-link"> <Link to="/show-vendors"><i className="fas fa-store-alt"></i> Vendor List </Link></li>
@@ -26,16 +16,8 @@ export default function Mainnav() {
               <li className="mainnav-link"> <Link to="/matsearch"> <i className="fas fa-street-view"></i> Search material and get details </Link></li>
           </ul>        
         </nav> 
-<<<<<<< HEAD
         
         
          </div>
     )
 }
-=======
-        </div>
-    )
-}
-
-
->>>>>>> cebe1101cd204bd9855d6b47e61a8b90901e19b8
