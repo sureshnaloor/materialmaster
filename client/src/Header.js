2bbfb6c918e2276c4d3a1abfe8d2315fc4c3f5a5
sureshnaloor <suresh.naloor@gmail.com>
2020-03-24 12:25:14 +0300

    nowcheck

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import logo from './images/JALLogo.png'
+import React from 'react';
+import logo from './images/JALLogo.png';
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+
 export default function Header() {
-    return ( 
+  return (
     <div>
-        <header className = "App-header">
-        <img src = { logo }/>  <h4> JAL SAP Database portal < /h4> 
-        <p> <span> Internal SAP Portal </span>developed for estimation and project teams for select MM reports and transactions</p >
-        </header> 
+      <header className='App-header'>
+        <div>
+          <img className='logo' src={logo} />
+          <h4> MM PORTAL - JAL International </h4>
+        </div>
+        <img src={img1} alt='pic1' />
+        <img src={img3} alt='pic2' />
+        <img src={img2} alt='pic3' />
+        <div>
+          <p>
+            {' '}
+            <span>Internal SAP Portal</span>
+          </p>
+          <h4>for estimation and project teams for Material data</h4>
+          <h6> Strictly for internal use only for authorised users</h6>
+        </div>
+      </header>
     </div>
-    )
+  );
 }
