b41d0b3d08935f7a74de8aff9153d0c270bda4b6
sureshnaloor <suresh.naloor@gmail.com>
2020-03-25 07:26:11 +0300

    nav changed

--- client/src/Mainnav.js
+++ client/src/Mainnav.js
@@ -10,10 +10,10 @@ export default function Mainnav() {
               <li className="mainnav-link"> <Link to="/"> <i className="fa fa-home" aria-hidden="true"></i> Home </Link></li>
               <li className="mainnav-link"> <Link to="/show-matgroups"> <i className="far fa-images"></i>Show matgroups </Link></li>
               <li className="mainnav-link"> <Link to="/show-vendors"><i className="fas fa-store-alt"></i> Vendor List </Link></li>
-              <li className="mainnav-link"> <Link to="/show-materials"> <i className="fas fa-search"></i>Materials List </Link></li>
+              {/* <li className="mainnav-link"> <Link to="/show-materials"> <i className="fas fa-search"></i>Materials List </Link></li> */}
               <li className="mainnav-link"> <Link to="/wbswise-inv"> <i className="fas fa-street-view"></i> Project inv value </Link></li>
               <li className="mainnav-link"> <Link to="/saleswise-inv"> <i className="fas fa-street-view"></i> Sales order inv value </Link></li>
-              <li className="mainnav-link"> <Link to="/matsearch"> <i className="fas fa-street-view"></i> Search material and get details </Link></li>
+              <li className="mainnav-link"> <Link to="/matsearch"> <i className="fas fa-search"></i> Search material and get details </Link></li>
           </ul>        
         </nav> 
         
--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './FrontPage.css'
 import PlantwiseCumulative from './Stock/PlantwiseCumulative'
 import ValuePlantwise from './Stock/ValuePlantwise';
-import BarChartComp from './Stock/BarChartcomp';
+// import BarChartComp from './Stock/BarC   hartcomp';
 import Top100value from './Stock/Top100value';
 import SalesStock from './SpecialStock/SalesStock';
 import ProjectStock from './SpecialStock/ProjectStock';
@@ -12,7 +12,7 @@ const FrontPage = () => {
     return (
         <div className="containerfp">
             <div className = "invfig"> <ValuePlantwise /></div>
-            <div className="invbarchart"> <BarChartComp /> </div>
+            {/* <div className="invbarchart"> <BarChartComp /> </div> */}
             <div className="invpiechart"> <PlantwiseCumulative /> </div>
              
             <div className="tophundrd"> <Top100value /> </div> 
