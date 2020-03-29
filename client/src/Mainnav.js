5fb5906a495cdc63cc9292eb3b4195c6aa09bf62
suresh unnimenon <suresh.unni.menon@gmail.com>
2020-03-29 11:25:23 +0300

    transferOfficelaptop

--- client/src/Mainnav.js
+++ client/src/Mainnav.js
@@ -8,51 +8,44 @@ export default function Mainnav() {
       <nav className='mainnav'>
         <ul className='mainnav-menu'>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/'>
-              {' '}
-              <i className='fa fa-home' aria-hidden='true'></i> Home{' '}
+              <i className='fa fa-home' aria-hidden='true'></i> Home
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/show-matgroups'>
-              {' '}
-              <i className='far fa-images'></i>Show matgroups{' '}
+              <i className='far fa-images'></i>Show matgroups
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/show-vendors'>
-              <i className='fas fa-store-alt'></i> Vendor List{' '}
+              <i className='fas fa-store-alt'></i> Vendor List
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/search-vendor'>
-              {' '}
-              <i className='fas fa-search'></i>Search vendor and get details{' '}
+              <i className='fas fa-search'></i>Search vendor and get details
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/wbswise-inv'>
-              {' '}
-              <i className='fas fa-street-view'></i> Project inv value{' '}
+              <i className='fas fa-street-view'></i> Project inv value
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/saleswise-inv'>
-              {' '}
-              <i className='fas fa-street-view'></i> Sales order inv value{' '}
+              <i className='fas fa-street-view'></i> Sales order inv value
             </Link>
           </li>
           <li className='mainnav-link'>
-            {' '}
             <Link to='/matsearch'>
-              {' '}
-              <i className='fas fa-search'></i> Search material and get details{' '}
+              <i className='fas fa-search'></i> Search material and get details
+            </Link>
+          </li>
+
+          <li className='mainnav-link'>
+            <Link to='/sign-in'>
+              <i className='fas fa-sign-in-alt'></i> Sign In
             </Link>
           </li>
         </ul>
