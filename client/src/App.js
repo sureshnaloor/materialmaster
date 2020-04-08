b7b72c137cb3b5675659fb13358c21ee53d2d4db
suresh unnimenon <suresh.unni.menon@gmail.com>
2020-04-08 08:53:51 +0300

    vendorsearh implemented draft

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Header from './Header';
 import Mainnav from './Mainnav';
 
-import FrontPage from './components/FrontPage'
+import FrontPage from './components/FrontPage';
 
-import ShowMaterialGroups from './components/Materials/MaterialGroups'
-import MatGroupwise from './components/Materials/MatGroupwise'
+import ShowMaterialGroups from './components/Materials/MaterialGroups';
+import MatGroupwise from './components/Materials/MatGroupwise';
 import CreateMaterial from './components/Materials/CreateMaterial';
 import ShowMaterials from './components/Materials/ShowMaterials.';
 import ShowMaterialDetails from './components/Materials/ShowMaterialDetails';
@@ -22,45 +22,63 @@ import ProjectStock from './components/SpecialStock/ProjectStock';
 import MaterialTransactions from './components/Materials/MaterialTransactions';
 import MaterialReceipts from './components/Materials/MaterialReceipts';
 import Matsearchfc from './components/Search/Matsearchfc';
+import Vendorsearch from './components/Search/Vendorsearch';
 
 class App extends Component {
-  render(){
-  return (
-    <Router>
-    <div className="App">
-      <Header />
-      <Mainnav />                
-      <div>
-        <Switch>
-          <Route exact path='/' component={FrontPage} />  
-          <Route path='/show-materials' component={ShowMaterials} />
-          <Route path='/create-material' component={CreateMaterial} />
-          <Route path='/edit-material/:id' component={UpdateMaterialInfo} />
-          <Route path='/show-material/:id' component={ShowMaterialDetails} />
-          <Route path='/purchases/:matcode' component={MaterialPurchases} />
-          <Route path='/matissuedocs/:matcode' component={MaterialTransactions} />
-          <Route path='/matreceiptdocs/:matcode' component={MaterialReceipts} />
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Header />
+          <Mainnav />
+          <div>
+            <Switch>
+              <Route exact path='/' component={FrontPage} />
+              <Route path='/show-materials' component={ShowMaterials} />
+              <Route path='/create-material' component={CreateMaterial} />
+              <Route path='/edit-material/:id' component={UpdateMaterialInfo} />
+              <Route
+                path='/show-material/:id'
+                component={ShowMaterialDetails}
+              />
+              <Route path='/purchases/:matcode' component={MaterialPurchases} />
+              <Route
+                path='/matissuedocs/:matcode'
+                component={MaterialTransactions}
+              />
+              <Route
+                path='/matreceiptdocs/:matcode'
+                component={MaterialReceipts}
+              />
 
-          <Route path='/show-matgroups' exact={true} component={ShowMaterialGroups} />
-          <Route path='/show-materialgwise/:mg' exact = {true} component={MatGroupwise} />          
+              <Route
+                path='/show-matgroups'
+                exact={true}
+                component={ShowMaterialGroups}
+              />
+              <Route
+                path='/show-materialgwise/:mg'
+                exact={true}
+                component={MatGroupwise}
+              />
 
-          <Route path='/show-vendors' component={ShowVendors} />
-          <Route path='/show-vendor/:id' component={ShowVendorDetails} />
+              <Route path='/show-vendors' component={ShowVendors} />
+              <Route path='/show-vendor/:id' component={ShowVendorDetails} />
+              <Route path='/search-vendor' exact component={Vendorsearch} />
 
-          <Route path='/plantwise-stk' component={PlantwiseCumulative} />
-          <Route path='/allmaterial-stk' component = {Top100value} />
+              <Route path='/plantwise-stk' component={PlantwiseCumulative} />
+              <Route path='/allmaterial-stk' component={Top100value} />
 
-          <Route path='/saleswise-inv' component = {SalesStock} />
-          <Route path='/wbswise-inv' component = {ProjectStock } />
+              <Route path='/saleswise-inv' component={SalesStock} />
+              <Route path='/wbswise-inv' component={ProjectStock} />
 
-          <Route path='/matsearch' exact = {true} component={Matsearchfc} />
-        </Switch>
-        
-      </div>  
-    </div>
-    </Router>
-      )
-    }
+              <Route path='/matsearch' exact={true} component={Matsearchfc} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
