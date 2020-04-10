4de015dcc795a220cc9b01868f38300ec493b7f8
suresh unnimenon <suresh.unni.menon@gmail.com>
2020-04-10 07:06:41 +0300

    startAuth0

--- client/src/components/Materials/MaterialGroups.js
+++ client/src/components/Materials/MaterialGroups.js
@@ -1,101 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import '../../App.css'
-import MatGroupwise from './MatGroupwise'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import '../../App.css';
+import MatGroupwise from './MatGroupwise';
 
-import axios from 'axios'
+import axios from 'axios';
 
 const RowMatgroupStyle = {
-    fontFamily: 'Roboto', 
-    color: "Maroon",
-    fontSize:"14px"  
-}
+  fontFamily: 'Roboto',
+  color: 'Maroon',
+  fontSize: '14px',
+};
 
 const RowMatgroupdescStyle = {
-    fontFamily: 'Roboto', 
-    color: "brown",
-    textAlign: "left",
-    fontSize:"14px",
-    fontWeight:"400"
-}
+  fontFamily: 'Roboto',
+  color: 'brown',
+  textAlign: 'left',
+  fontSize: '14px',
+  fontWeight: '400',
+};
 
 const containerAll = {
-    margin: "0",
-    marginLeft: "10px",
-    padding: "0",
-    display: "flex",
-    flexDirection: "row"
-}
+  margin: '0',
+  marginLeft: '10px',
+  padding: '0',
+  display: 'flex',
+  flexDirection: 'row',
+};
 
 const invTableStyle1 = {
-    float:"left",    
-    width: "40vw",
-    fontFamily:"'Courgette', cursive"
-}
+  float: 'left',
+  width: '40vw',
+  fontFamily: "'Courgette', cursive",
+};
 
 const ShowMaterialGroups = () => {
-    const [matgroups, setMatgroups] = useState([])
-    const [isLoading, setLoading] = useState(true) 
-    
-    const GetSortOrder = (objkey) =>  {  
-        return function(a, b) {  
-            if (a[objkey] > b[objkey]) {  
-                return 1;  
-            } else if (a[objkey] < b[objkey]) {  
-                return -1;  
-            }  
-            return 0;   }  
-    }   
+  const [matgroups, setMatgroups] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-    const fetchMatgroups = async () => {           
-        const resp = await axios("http://localhost:5000/api/matcodes/matgroups") 
-        const matgrlist = resp.data.sort(GetSortOrder("groupCode") )
-        setMatgroups(matgrlist)   
-        setLoading(false)    
-    }
+  const GetSortOrder = (objkey) => {
+    return function (a, b) {
+      if (a[objkey] > b[objkey]) {
+        return 1;
+      } else if (a[objkey] < b[objkey]) {
+        return -1;
+      }
+      return 0;
+    };
+  };
 
-    useEffect(() => {
-        fetchMatgroups(matgroups);
-    }, [matgroups]);
+  const fetchMatgroups = async () => {
+    const resp = await axios('http://localhost:5000/api/matcodes/matgroups');
+    const matgrlist = resp.data.sort(GetSortOrder('groupCode'));
+    setMatgroups(matgrlist);
+    setLoading(false);
+  };
 
-    
-    if (isLoading) {
-        return (<><img src = {require('../../images/2.gif')} alt="loading"/> <h5> Loading.....</h5></>)
-    }
-    else {
-    return (      
-        
-        <div style={containerAll}>   <table style = {invTableStyle1} className="table table-hover table-striped table-sm">
-                  <thead className="black white-text">
-                        <tr>
-                            <th ></th> 
-                            <th scope="col">Serial No#</th>
-                            <th scope="col">Group Code</th>
-                            <th scope="col" >Material group name</th>                           
-                        </tr>
-                  </thead>  
-                  <tbody>    
-                                  
-                       { matgroups.map((mat, index) =>     
-                           <Router> 
-                            <tr key={mat._id}>
-                                
-                            <th scope="row"><Link to = {`/show-materialgwise/${mat.groupCode}`}><i className="fas fa-plus-square"></i> </Link>
-                           
-                            </th>
-                            <td> {index+1} </td>
-                            <td style={RowMatgroupStyle}>{mat.groupCode}</td>
-                            <td style={RowMatgroupdescStyle}>{mat.groupName.toUpperCase()}</td>                            
-                        </tr>  
-                        <Switch> <Route path="/show-materialgwise/:mg" children={<MatGroupwise />} /></Switch>
-                        </Router>  
-                        )}
-                        
-                  </tbody>          
-            </table>                              
-        </div>        
-    )}
-    }
-    
-    export default ShowMaterialGroups
+  useEffect(() => {
+    fetchMatgroups(matgroups);
+  }, [matgroups]);
 
+  if (isLoading) {
+    return (
+      <>
+        <img src={require('../../images/2.gif')} alt='loading' />{' '}
+        <h5> Loading.....</h5>
+      </>
+    );
+  } else {
+    return (
+      <div style={containerAll}>
+        {' '}
+        <table
+          style={invTableStyle1}
+          className='table table-hover table-striped table-sm'
+        >
+          <thead className='black white-text'>
+            <tr>
+              {/* <th ></th>  */}
+              <th scope='col'>Serial No#</th>
+              <th scope='col'>Group Code</th>
+              <th scope='col'>Material group name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {matgroups.map((mat, index) => (
+              <Router>
+                <tr key={mat._id}>
+                  {/* <th scope="row"><Link to = {`/show-materialgwise/${mat.groupCode}`}><i className="fas fa-plus-square"></i> </Link> */}
+
+                  {/* </th> */}
+                  <td> {index + 1} </td>
+                  <td style={RowMatgroupStyle}>{mat.groupCode}</td>
+                  <td style={RowMatgroupdescStyle}>
+                    {mat.groupName.toUpperCase()}
+                  </td>
+                </tr>
+                {/* <Switch> <Route path="/show-materialgwise/:mg" children={<MatGroupwise />} /></Switch> */}
+              </Router>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
+
+export default ShowMaterialGroups;
--- client/src/components/Vendors/VendorCard.js
+++ client/src/components/Vendors/VendorCard.js
@@ -1,89 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../App.css'
+import '../../App.css';
 
 const RowVendorStyle = {
-    fontFamily: "'Open Sans', sans-serif", 
-    color: "Maroon",
-    fontSize:"14px"  
-}
+  fontFamily: "'Open Sans', sans-serif",
+  color: 'Maroon',
+  fontSize: '14px',
+};
 
 const RowVendorNameStyle = {
-    fontFamily: "'Open Sans', sans-serif",
-    color: "brown",
-    textAlign: "left",
-    fontSize:"14px"
-}
+  fontFamily: "'Open Sans', sans-serif",
+  color: 'brown',
+  textAlign: 'left',
+  fontSize: '14px',
+};
 const RowVendorOtherStyle1 = {
-    fontFamily: "'Open Sans', sans-serif",
-    color: "blue",
-    textAlign: "left",
-    fontSize:"14px"
-}
+  fontFamily: "'Open Sans', sans-serif",
+  color: 'blue',
+  textAlign: 'left',
+  fontSize: '14px',
+};
 
 const RowVendorOtherStyle2 = {
-    fontFamily: "'Open Sans', sans-serif",
-    color: "teal",
-    textAlign: "left",
-    fontSize:"14px"
-}
+  fontFamily: "'Open Sans', sans-serif",
+  color: 'teal',
+  textAlign: 'left',
+  fontSize: '14px',
+};
 
 const invTableStyle = {
-    position: "relative",
-    left: "5px",
-    width: "60vw",
-    fontFamily:"'Courgette', cursive"    
-}
+  position: 'relative',
+  left: '5px',
+  width: '60vw',
+  fontFamily: "'Courgette', cursive",
+};
 
 const icon = {
-    fontSize: "12px"
-}
+  fontSize: '12px',
+};
 
 const VendorCard = (props) => {
-    const {vendors, isLoading} = props
+  const { vendors, isLoading } = props;
 
-    if (isLoading) {
-        return (<><img src = {require('../../images/2.gif')} alt="loading"/> <h5> Loading.....</h5></>)
-    }
-    else {
+  if (isLoading) {
     return (
-        <>   
-        <table style = {invTableStyle} className="table table-hover table-striped table-sm">
-                  <thead className="black white-text">
-                        <tr>
-                            <th scope="col"> </th>
-                            <th scope="col">Serial No#</th>
-                            <th scope="col">Vendor Code</th>
-                            <th scope="col" >Vendor Name </th>
-                            <th scope="col">Country</th>
-                            <th scope="col">City</th>
-                            <th scope="col">Telephone</th>
-                            <th scope="col">Sales Person</th>
-                            <th scope="col">Mobile No</th>
-                        </tr>
-                  </thead>  
-                  <tbody>    
-                  { vendors.map((vendor, index) =>                 
-                  
-                  <tr key={vendor._id}>
-                      <th scope="row"><Link to = {`/show-vendor/${vendor._id}`}><i style={icon} className="fas fa-binoculars"></i> </Link></th>
-                      <td style={RowVendorStyle}> {index+1} </td>
-                      <td style={RowVendorStyle}>{vendor.VendorNumber}</td>
-                      <td style={RowVendorNameStyle}>{vendor.Name}</td>
-                      <td style={RowVendorOtherStyle1}>{vendor.Country}</td>
-                      <td style={RowVendorOtherStyle2}>{vendor.City}</td>
-                      <td style={RowVendorOtherStyle1}>{vendor.Telephone}</td>
-                      <td style={RowVendorOtherStyle2}>{vendor.Salesperson}</td>
-                      <td style={RowVendorOtherStyle1}>{vendor.MobileNo}</td>
-                  </tr>   
-                  )}
-            </tbody>          
-      </table>                                   
-                  
-        </>
-    )
+      <>
+        <img src={require('../../images/2.gif')} alt='loading' />{' '}
+        <h5> Loading.....</h5>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <table
+          style={invTableStyle}
+          className='table table-hover table-striped table-sm'
+        >
+          <thead className='black white-text'>
+            <tr>
+              <th scope='col'> </th>
+              <th scope='col'>Serial No#</th>
+              <th scope='col'>Vendor Code</th>
+              <th scope='col'>Vendor Name </th>
+              <th scope='col'>Country</th>
+              <th scope='col'>City</th>
+              <th scope='col'>Telephone</th>
+              <th scope='col'>Sales Person</th>
+              <th scope='col'>Mobile No</th>
+            </tr>
+          </thead>
+          <tbody>
+            {vendors.map((vendor, index) => (
+              <tr key={vendor._id}>
+                {/* <th scope="row"><Link to = {`/show-vendor/${vendor._id}`}><i style={icon} className="fas fa-binoculars"></i> </Link></th> */}
+                <td> </td>
+                <td style={RowVendorStyle}> {index + 1} </td>
+                <td style={RowVendorStyle}>{vendor.VendorNumber}</td>
+                <td style={RowVendorNameStyle}>{vendor.Name}</td>
+                <td style={RowVendorOtherStyle1}>{vendor.Country}</td>
+                <td style={RowVendorOtherStyle2}>{vendor.City}</td>
+                <td style={RowVendorOtherStyle1}>{vendor.Telephone}</td>
+                <td style={RowVendorOtherStyle2}>{vendor.Salesperson}</td>
+                <td style={RowVendorOtherStyle1}>{vendor.MobileNo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+};
 
-}
-}
-
-export default VendorCard
+export default VendorCard;
