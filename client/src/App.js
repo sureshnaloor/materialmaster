8f5538a0362f1497a7c94e7c4b709397bb683863
suresh unnimenon <suresh.unni.menon@gmail.com>
2020-04-12 19:34:16 +0300

    new branch for  OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Mainnav from './Mainnav';
@@ -23,60 +29,81 @@ import MaterialTransactions from './components/Materials/MaterialTransactions';
 import MaterialReceipts from './components/Materials/MaterialReceipts';
 import Matsearchfc from './components/Search/Matsearchfc';
 import Vendorsearch from './components/Search/Vendorsearch';
+import Callback from './Callback';
+import Auth from './Auth/Auth';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
   render() {
     return (
-      <Router>
-        <div className='App'>
-          <Header />
-          <Mainnav />
-          <div>
-            <Switch>
-              <Route exact path='/' component={FrontPage} />
-              <Route path='/show-materials' component={ShowMaterials} />
-              <Route path='/create-material' component={CreateMaterial} />
-              <Route path='/edit-material/:id' component={UpdateMaterialInfo} />
-              <Route
-                path='/show-material/:id'
-                component={ShowMaterialDetails}
-              />
-              <Route path='/purchases/:matcode' component={MaterialPurchases} />
-              <Route
-                path='/matissuedocs/:matcode'
-                component={MaterialTransactions}
-              />
-              <Route
-                path='/matreceiptdocs/:matcode'
-                component={MaterialReceipts}
-              />
+      <div className='App'>
+        <Header />
+        <Mainnav auth={this.auth} />
+        <div>
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={(props) => <FrontPage auth={this.auth} {...props} />}
+            />
+            <Route
+              exact
+              path='/callback'
+              render={(props) => <Callback auth={this.auth} {...props} />}
+            />
+            <Route path='/show-materials' component={ShowMaterials} />
+            <Route path='/create-material' component={CreateMaterial} />
+            <Route path='/edit-material/:id' component={UpdateMaterialInfo} />
+            <Route path='/show-material/:id' component={ShowMaterialDetails} />
+            <Route path='/purchases/:matcode' component={MaterialPurchases} />
+            <Route
+              path='/matissuedocs/:matcode'
+              component={MaterialTransactions}
+            />
+            <Route
+              path='/matreceiptdocs/:matcode'
+              component={MaterialReceipts}
+            />
 
-              <Route
-                path='/show-matgroups'
-                exact={true}
-                component={ShowMaterialGroups}
-              />
-              <Route
-                path='/show-materialgwise/:mg'
-                exact={true}
-                component={MatGroupwise}
-              />
+            <Route
+              path='/show-matgroups'
+              exact={true}
+              component={ShowMaterialGroups}
+            />
+            <Route
+              path='/show-materialgwise/:mg'
+              exact={true}
+              component={MatGroupwise}
+            />
 
-              <Route path='/show-vendors' component={ShowVendors} />
-              <Route path='/show-vendor/:id' component={ShowVendorDetails} />
-              <Route path='/search-vendor' exact component={Vendorsearch} />
+            <Route path='/show-vendors' component={ShowVendors} />
+            <Route path='/show-vendor/:id' component={ShowVendorDetails} />
+            <Route path='/search-vendor' exact component={Vendorsearch} />
 
-              <Route path='/plantwise-stk' component={PlantwiseCumulative} />
-              <Route path='/allmaterial-stk' component={Top100value} />
+            <Route path='/plantwise-stk' component={PlantwiseCumulative} />
+            <Route path='/allmaterial-stk' component={Top100value} />
 
-              <Route path='/saleswise-inv' component={SalesStock} />
-              <Route path='/wbswise-inv' component={ProjectStock} />
+            <Route path='/saleswise-inv' component={SalesStock} />
+            {/* <Route path='/wbswise-inv' component={ProjectStock} /> */}
+            <Route
+              exact
+              path='/wbswise-inv'
+              render={(props) =>
+                this.auth.isAuthenticated() ? (
+                  <ProjectStock auth={this.auth} {...props} />
+                ) : (
+                  <Redirect to='/' />
+                )
+              }
+            />
 
-              <Route path='/matsearch' exact={true} component={Matsearchfc} />
-            </Switch>
-          </div>
+            <Route path='/matsearch' exact={true} component={Matsearchfc} />
+          </Switch>
         </div>
-      </Router>
+      </div>
     );
   }
 }
---
+++ client/src/Auth/Auth.js
@@ -0,0 +1,76 @@
+import auth0 from 'auth0-js';
+export default class Auth {
+  constructor(history) {
+    this.history = history;
+    this.userProfile = null;
+
+    this.auth0 = new auth0.WebAuth({
+      domain: 'dev-0nw05e1d.auth0.com',
+      clientID: '0cqCC4vC0kL86gard9CoPvNZSfJg5MJb',
+      redirectUri: 'http://localhost:3000/callback',
+      responseType: 'token id_token',
+      scope: 'openid profile email',
+    });
+  }
+
+  login = () => {
+    this.auth0.authorize();
+  };
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push('/');
+        console.log('session storage set');
+      } else if (err) {
+        this.history.push('/');
+        alert(`Error: ${err.error} check console for further details`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = (authResult) => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  };
+
+  isAuthenticated = () => {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  };
+
+  logout = () => {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    localStorage.clear();
+    this.userProfile = null;
+    this.history.push('/');
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      returnTo: 'http://localhost:3000',
+    });
+  };
+
+  getAccessToken = () => {
+    const accesstoken = localStorage.getItem('access_token');
+    if (!accesstoken) {
+      throw new Error('access token not found');
+    }
+    return accesstoken;
+  };
+
+  getProfile = (cb) => {
+    if (this.userProfile) return cb(this.userProfile);
+    this.auth0.client.userInfo(this.getAccessToken(), (err, profile) => {
+      if (profile) this.userProfile = profile;
+      cb(profile, err);
+    });
+  };
+}
